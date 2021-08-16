b6b1f7bcbc43cd541aadd9b8727611522ac08856
briannaxlee <briannaxlee@gmail.com>
2021-08-15 23:59:24 -0400

    Create StopWatch.java

---
+++ final question/StopWatch.java
@@ -0,0 +1,37 @@
+package finalquestion;
+
+public class StopWatch
+{
+	private long startTime;
+	private long endTime;
+
+	public StopWatch()
+	{
+		startTime = System.currentTimeMillis();
+	}
+
+	public void start()
+	{
+		startTime = System.currentTimeMillis();
+	} 
+
+	public void stop()
+	{
+		endTime = System.currentTimeMillis();
+	}
+
+	public long getElapsedTime()
+	{
+		return getEndTime() - getStartTime();
+	}
+
+	public long getStartTime()
+	{
+		return startTime;
+	}
+
+	public long getEndTime()
+	{
+		return endTime;
+	}
+}
